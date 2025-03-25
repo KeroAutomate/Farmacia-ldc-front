c2f880000ee14505d0a3007082efdd5ac97fd16e
José Gabriel <keroggca@gmail.com>
2025-03-25 00:03:03 -0300

    express-rate-limit removed

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,23 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import rateLimit from 'express-rate-limit';
 export const runtime = 'edge';
 
 // Configuração do rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 10, // Máximo de 10 requisições por janela de tempo
-  message: { success: false, message: 'Muitas tentativas de login. Tente novamente mais tarde.' },
-  standardHeaders: true, // Retorna informações de rate limit nos headers
-  legacyHeaders: false, // Desabilita cabeçalhos obsoletos
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutos
+//   max: 10, // Máximo de 10 requisições por janela de tempo
+//   message: { success: false, message: 'Muitas tentativas de login. Tente novamente mais tarde.' },
+//   standardHeaders: true, // Retorna informações de rate limit nos headers
+//   legacyHeaders: false, // Desabilita cabeçalhos obsoletos
+// });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Middleware de rate limit
-  await new Promise((resolve, reject) => {
-    limiter(req, res, (result: unknown) =>
-      result instanceof Error ? reject(result) : resolve(result)
-    );
-  });
+
 
   if (req.method === 'POST') {
     const { name, senha } = req.body;
